a9b7e37a5107f6005491eaf54116236c9cb55b3c
Tan Yin Loo <tianhai82@yahoo.com>
2023-09-02 11:41:32 +0800

    add air

--- views/views.go
+++ views/views.go
@@ -5,14 +5,14 @@ import (
 	"html/template"
 )
 
-//go:embed pages/*
+//go:embed pages/**
 var Files embed.FS
 
 //go:embed assets/*
 var Assets embed.FS
 
 func ParseFS() *template.Template {
-	return template.Must(template.ParseFS(Files, "pages/*"))
+	return template.Must(template.ParseFS(Files, "pages/**/*"))
 }
 func ParseGlob() *template.Template {
 	return template.Must(template.ParseGlob("views/pages/**/*"))
